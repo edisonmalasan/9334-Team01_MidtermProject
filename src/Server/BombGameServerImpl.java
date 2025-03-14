6f65e0ced9b2366e7c6da2752506e066224798ca
INSTRUCT-D522lab <2240879@slu.edu.ph>
2025-03-14 09:32:43 +0800

    Added methods

--- src/Server/BombGameServerImpl.java
+++ src/Server/BombGameServerImpl.java
@@ -1,34 +1,200 @@
 package Server;
 
+import Client.model.PlayerModel;
+import Server.controller.QuestionController;
+import Server.controller.XMLStorageController;
+import Server.handler.ClientHandler;
+import Server.model.LeaderboardEntryModelServer;
+import common.AnsiFormatter;
+import common.Response;
+import common.model.QuestionModel;
 import utility.BombGameServer;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class BombGameServerImpl extends UnicastRemoteObject implements BombGameServer {
+
+    private String fileName;
+    private static final Logger logger = Logger.getLogger(ClientHandler.class.getName());
+
+    static {
+        AnsiFormatter.enableColorLogging(logger);
+    }
     public BombGameServerImpl() throws RemoteException {
     }
 
     @Override
-    public void getQuestionsPerCategory() throws RemoteException {
+    public void getQuestionsPerCategory(String category) throws RemoteException {
+        logger.info("Server received question request for category: " + category);
 
+        QuestionController questionController = new QuestionController();
+        List<QuestionModel> questions = questionController.getQuestionsByCategory(category);
+
+        if (questions.isEmpty()) {
+            logger.warning("No questions found for category: " + category);
+        //    return new Response(false, "No questions found for category: " + category, null);
+        }
+
+        logger.info("Question retrieved successfully for category: " + category);
+        // return new Response(true, "Question retrieved successfully.", questions);
     }
 
     @Override
-    public void getLeaderboards() throws RemoteException {
+    public void getLeaderboards(List<?> leaderboardList, String jsonFile) throws RemoteException {
+        try {
+            if (leaderboardList == null) {
+                logger.severe("Received null player data.");
+               // return new Response(false, "Received null player data.", null);
+            }
 
+            if (jsonFile.equals("classic")) {
+                fileName = "data/classic_leaderboard.xml";
+            } else {
+                fileName = "data/endless_leaderboard.xml";
+            }
+            List<LeaderboardEntryModelServer> leaderboard = XMLStorageController.loadLeaderboardFromXML(fileName);
+            logger.info("Returning leaderboard data.");
+           // return new Response(true, "Leaderboard displayed successfully.", leaderboard);
+        } catch (Exception e) {
+            logger.severe("Error retrieving leaderboard: " + e.getMessage());
+            // return new Response(false, "Error retrieving leaderboard: " + e.getMessage(), null);
+        }
     }
 
     @Override
     public void getQuestionsList() throws RemoteException {
+        logger.info("Server received request for question list");
 
+        QuestionController questionController = new QuestionController();
+        List<QuestionModel> allQuestions = questionController.getQuestionsList();
+
+        if (allQuestions.isEmpty()) {
+            logger.warning("No question list found.");
+         //   return new Response(false, "No question list found", null);
+        }
+
+        logger.info("Question list retrieved successfully");
+       // return new Response(true, "Question list retrieved successfully.", allQuestions);
     }
-
     @Override
-    public void updatePlayerScore() throws RemoteException {
+    public void updatePlayerScore(PlayerModel player) throws RemoteException {
+        try {
+            if (player == null) {
+                logger.severe("Received null player data.");
+                //return new Response(false, "Received null player data.", null);
+            }
 
+            String usernameLower = player.getName().toLowerCase();
+            int newScore = player.getScore();
+            logger.info("Updating player score: " + usernameLower + " with score: " + newScore);
+
+            fileName = "data/classic_leaderboard.xml";
+            List<LeaderboardEntryModelServer> leaderboard = XMLStorageController.loadLeaderboardFromXML(fileName);
+
+            if (player.getName().endsWith("  ")) {
+                player.getName().trim();
+                player.setName(player.getName());
+                fileName = "data/endless_leaderboard.xml";
+                leaderboard = XMLStorageController.loadLeaderboardFromXML(fileName);
+            }
+
+            boolean found = false;
+            for (LeaderboardEntryModelServer entry : leaderboard) {
+                if (entry.getPlayerName().equalsIgnoreCase(usernameLower)) {
+                    // compare the new score with the existing score
+                    if (newScore > entry.getScore()) {
+                        entry.setScore(newScore); // update the score only if the new score is higher
+                        logger.info("Updated score for player: " + usernameLower + " to: " + newScore);
+                    } else {
+                        logger.info("New score is not higher. Keeping the existing score: " + entry.getScore());
+                    }
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                leaderboard.add(new LeaderboardEntryModelServer(usernameLower, newScore));
+                logger.info("Added new player to leaderboard: " + usernameLower + " with score: " + newScore);
+            }
+
+            XMLStorageController.saveLeaderboardToXML(fileName, leaderboard);
+
+            logger.info("Player score updated successfully.");
+            //return new Response(true, "Player score updated successfully.", null);
+        } catch (Exception e) {
+            logger.severe("Error updating player score: " + e.getMessage());
+            //return new Response(false, "Error updating player score: " + e.getMessage(), null);
+        }
     }
 
+    public void addQuestion(QuestionModel question) throws RemoteException {
+        try {
+            logger.info("Server received request for removing question in database");
+            if (question == null) {
+                logger.severe("Received null question data.");
+               // return new Response(false, "Received null question data.", null);
+            }
+
+            fileName = "data/questions.xml";
+            List<QuestionModel> questionList = XMLStorageController.loadQuestionsFromXML(fileName);
+
+            boolean found = false;
+            for (QuestionModel questionModel : questionList) {
+                if (question.equals(questionModel)) {
+                    logger.info("Removed question from database: " + questionModel.getCategory() + " with text: " + questionModel.getQuestionText());
+                    questionList.remove(questionModel);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                questionList.add(question);
+                logger.info("Question added to database: " + question.getCategory() + " with text: " + question.getQuestionText());
+            }
+
+            XMLStorageController.saveQuestionsToXML(fileName, questionList);
+            logger.info("Question database updated successfully.");
+          //  return new Response(true, "Question database updated successfully.", null);
+        } catch (Exception e) {
+            logger.severe("Error updating: " + e.getMessage());
+          //  return new Response(false, "Error updating question database: " + e.getMessage(), null);
+        }
+    }
+
+    public void updateLeaderboard(LeaderboardEntryModelServer leaderboardEntry) throws RemoteException {
+        try {
+            logger.info("Server received request for removal of entry in leaderboard");
+            if (leaderboardEntry == null) {
+                logger.severe("Received null leaderboard entry data.");
+              //  return new Response(false, "Received null leaderboard entry data.", null);
+            }
+
+            fileName = "data/classic_leaderboard.xml";
+            List<LeaderboardEntryModelServer> leaderboard = XMLStorageController.loadLeaderboardFromXML(fileName);
+
+            if (leaderboardEntry.getPlayerName().endsWith("  ")) {
+                fileName = "data/endless_leaderboard.xml";
+                leaderboard = XMLStorageController.loadLeaderboardFromXML(fileName);
+            }
+            for (LeaderboardEntryModelServer entry : leaderboard) {
+                if (leaderboardEntry.equals(entry)) {
+                    logger.info("Removed entry from leaderboard: " + entry.getPlayerName() + " with score: " + entry.getScore());
+                    leaderboard.remove(entry);
+                    break;
+                }
+            }
+            XMLStorageController.saveLeaderboardToXML(fileName, leaderboard);
+            logger.info("Leaderboard updated successfully.");
+           // return new Response(true, "Leaderboard updated successfully.", null);
+        } catch (Exception e) {
+            logger.severe("Error removing entry from leaderboard: " + e.getMessage());
+           // return new Response(false, "Error removing entry from leaderboard: " + e.getMessage(), null);
+        }
+    }
     @Override
     public void login() throws RemoteException {
 
--- src/utility/BombGameServer.java
+++ src/utility/BombGameServer.java
@@ -1,21 +1,32 @@
 package utility;
 
+import Client.model.PlayerModel;
+import Server.model.LeaderboardEntryModelServer;
+import common.model.QuestionModel;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface BombGameServer extends Remote {
 
     // Load questions for given category
-    public void getQuestionsPerCategory() throws RemoteException;
+    public void getQuestionsPerCategory(String category) throws RemoteException;
 
     // Get data for leaderboards (classic and endless)
-    public void getLeaderboards() throws RemoteException;
+    public void getLeaderboards(List<?> list, String fileName) throws RemoteException;
 
     // Get the list of all questions
     public void getQuestionsList() throws RemoteException;
 
     // Update score of existing player
-    public void updatePlayerScore() throws RemoteException;
+    public void updatePlayerScore(PlayerModel player) throws RemoteException;
+
+    // Add question to database
+    public void addQuestion(QuestionModel question) throws RemoteException;
+
+    // Update leaderboards data
+    public void updateLeaderboard(LeaderboardEntryModelServer leaderboardEntry) throws RemoteException;
 
     // Login account (player and admin)
     public void login() throws RemoteException;
