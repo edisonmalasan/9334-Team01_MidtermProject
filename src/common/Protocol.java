ae0e6ba498bd9e7df6111171e3af76d3379d159c
Eruki <malasanedison1@gmail.com>
2025-03-25 17:34:44 +0800

    changed EXIT to LOGOUT in main menu

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -5,5 +5,5 @@ package common;
  */
 public class Protocol {
     public static final int PORT_NUMBER = 1099;
-    public static final String IP_ADDRESS = "127.0.0.1"; // App.java IP
+    public static final String IP_ADDRESS = "localhost"; // App.java IP
 }
